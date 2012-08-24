f77fafa7f1a9c5f5ac64f123455ebf926be3e040
Christopher Heriot <cheriot@gmail.com>
2012-08-23 23:58:38 -0400

    Clone vundle to where vim will expect it.

--- activate.sh
+++ activate.sh
@@ -5,3 +5,6 @@ for dotfile in bash_aliases bash_profile bashrc gitconfig vimrc
 do
   ln -s $dotfiles_dir/$dotfile ~/.$dotfile
 done
+
+mkdir -p ~/.vim/bundle
+git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
